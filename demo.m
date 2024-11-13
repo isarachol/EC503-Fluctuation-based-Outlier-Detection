e964662921a452ef62527d65af91aeae7a5732ed
Isara Cholaseuk <tand.isara2001@gmail.com>
2024-11-12 22:21:57 -0500

    Updated demo, extract dataset
    Made extract_dataset a script function to extract dataset before using in demo

--- demo.m
+++ demo.m
@@ -1,20 +1,29 @@
 function [outputArg1,outputArg2] = main(GraphNumber,k)
+%% cd
 dataset_directory = 'C:\Users\tandi\OneDrive\Desktop\Classes\2024 Fall\EC 503 A1 B1(recite)\Project\Datasets';
 graph_directory = 'C:\Users\tandi\OneDrive\Desktop\Classes\2024 Fall\EC 503 A1 B1(recite)\Project\Graph\';
 cd(dataset_directory);
-file_name = 'Covid Data.csv';
-n = 100;
+%% load data
+file_name = 'PNDB.mat';
+n_max = 1000;
 tic
-X = extract_dataset_csv(file_name, n, 'Covid Data.mat');
+X = load(file_name).Dataset;
+[n, ~] = size(X);
+if n>n_max % limit data to n_max points
+    n = n_max;
+    X = X(1:n, :);
+end
+fprintf("Using %d data points", n);
+%% FBOD
 %hyper parameter
 GraphNumber=10;
 k=60;
 Abnormal_number=20; %abnormal number (how to select this number?)
-Label=load('Label_wine.txt');
-ADLabels=load('Label_wine.txt');
+Label=load('PNDB_labels.mat').Labels;
+ADLabels=load('PNDB_labels.mat').Labels; % difference between Label and ADLabels?
 cd (graph_directory)
 delete *.txt
-GG_RandomLink(X,GraphNumber,k); % save txt files to Fluctuation-based-Outlier-Detection-main\Graph
+GG_RandomLink(X,GraphNumber,k); % save txt files to \Graph
 FVP(X',GraphNumber,Abnormal_number,ADLabels,Label,k);
 toc
 end
--- extract_dataset_csv.m
+++ extract_dataset_csv.m
@@ -1,31 +1,41 @@
-function dataset = extract_dataset_csv(file_name, n_max, save_file_name)
+% This function can be run here, no calling needed
+
+function [Dataset_out, Labels_out] = extract_dataset_PNDB_csv(file_name, n_max, label_col, save_files)
     arguments
-        file_name string
-        n_max int8
-        save_file_name char = '' % this is optional
+        file_name string = "PNDB.csv"
+        n_max int32 = 1000
+        label_col int8 = 8
+        save_files logical = 1 % this is optional
     end
+    %% cd
+    dataset_directory = 'C:\Users\tandi\OneDrive\Desktop\Classes\2024 Fall\EC 503 A1 B1(recite)\Project\Datasets';
+    cd(dataset_directory);
     %% read data
     raw_data=readtable(file_name); % table object
     [n, dim] = size(raw_data);
     if n>n_max % limit data to n_max points
         n=n_max;
     end
-    dataset = zeros(n, dim);
+    Dataset = zeros(n, dim-1);
+    Labels = raw_data{1:n,label_col};
 
     %% copy data and convert to numbers if necessary
     for c=1:dim
-        if iscell(raw_data{:,c}) % if the column is text
-            for r=1:n
-                % convert data to char and if it contains 'no', set it to 0
-                % otherwise 1
-                dataset(r,c) = double(contains(lower(char(raw_data{r,c})), 'no')); 
+        if c ~= label_col
+            if iscell(raw_data{:,c}) % if the column is text
+                for r=1:n
+                    % convert data to char and if it contains 'no', set it to 0
+                    % otherwise 1
+                    Dataset(r,c) = double(contains(lower(char(raw_data{r,c})), 'no')); 
+                end
+            else % if the column is number
+                Dataset(:,c) = raw_data{1:n,c};
             end
-        else % if the column is number
-            dataset(:,c) = raw_data{1:n,c};
         end
     end
     %% (OPTIONAL) save to .mat
-    if ~isempty(save_file_name)
-        save(save_file_name, "dataset");
+    if ~isempty(save_files)
+        save('PNDB.mat', "Dataset");
+        save('PNDB_labels.mat', "Labels");
     end
 end
